2bc2df3819dba1fe60cafad1f24faf36809f41cc
Galina684 <galina684@gmail.com>
2023-10-27 18:28:43 +0300

    Домашка инет магазин

---
+++ src/main/java/pro/sky/inetshop/InetShopApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.inetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InetShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InetShopApplication.class, args);
+	}
+
+}
